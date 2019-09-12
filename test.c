9cfa145b44182a45904986fdf1948633be587f4f
MrSklif1337 <48600120+MrSklif1337@users.noreply.github.com>
2019-09-12 10:04:38 +0300

    Update test.c

--- test.c
+++ test.c
@@ -25,3 +25,20 @@ void insert(double *array, int count_of_numbers, double result, int position)
     insert_in_place(array, count_of_numbers + 2, 1, result);
     *(array + count_of_numbers + INSERT_INDEX - 1) = result;
 }
+
+void check_insert_in_place()
+{
+	double array[6] = {1., 2., 3., 4., 5., 0.}
+	double result = 10.;
+	insert_in_place(array, 6, 3, result);
+	double check_array[6] = {1., 2., result, 3., 4., 5.};
+	int count = 0;
+	for (int i = 0; i < 6; i++)
+		if (array[i] == check_array[i])
+			count++;
+	if (count == 6)
+		printf("Correct work of inster in place\n";
+	
+	
+
+}
